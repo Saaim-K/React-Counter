5330d918f2cc8cfff7f5c355943f399c3400db0b
Saaim Khan <89778620+Saaim-K@users.noreply.github.com>
2022-10-17 23:19:59 +0500

    Counter

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [counter, changeCounter] = useState(0);
+
+
+
+  return (
+    <>
+      <h1>Counter {counter}</h1>
+
+      <button className="custom-btn btn-12" onClick={() =>changeCounter(counter + 1)}><span>CLICK!</span><span>INCREMENT</span></button>
+      <button className="custom-btn btn-12" onClick={() =>changeCounter(counter - 1)}><span>CLICK!</span><span>DECREMENT</span></button>
+    </>
+  );
+}
+
+export default App;
