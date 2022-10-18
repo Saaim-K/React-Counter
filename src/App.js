c039c4b32e34e61a0ee0ea9b01e91db0bb4439b7
Saaim-K <saaimkhan221@gmail.com>
2022-10-19 01:44:12 +0500

    Counter

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import { useState } from 'react';
 
 function App() {
   const [counter, changeCounter] = useState(0);
-  const {isLit,setLit}=useState(true)
+  // const { isLit, setLit } = useState(true)
 
-
+  const increment = () => changeCounter(counter + 1)
+  const decrement = () => changeCounter(counter - 1)
 
   return (
     <>
       <div className='navbar'>
-        <h3>Counter {isLit}</h3>
+        {/* <h3>Counter {isLit}</h3> */}
 
       </div>
       <div className='counter'>
         <h1>Counter {counter}</h1>
         <div className='btn'>
-          <button className="custom-btn btn-12" onClick={() => changeCounter(counter + 1)}><span> + 1 </span><span>INCREMENT</span></button>
-          <button className="custom-btn btn-12" onClick={() => changeCounter(counter - 1)}><span> - 1 </span><span>DECREMENT</span></button>
+          <button className="custom-btn btn-12" onClick={increment}><span> + 1 </span><span>INCREMENT</span></button>
+          <button className="custom-btn btn-12" onClick={decrement}><span> - 1 </span><span>DECREMENT</span></button>
         </div>
       </div>
     </>
