403b8565a1436f4b599d5028eef303f63ddcc06f
Saaim-K <saaimkhan221@gmail.com>
2022-10-19 03:01:40 +0500

    Counter

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { useState } from 'react';
 
 function App() {
   const [counter, changeCounter] = useState(0);
-  // const { isLit, setLit } = useState(true)
+  const [isLit, setLit] = useState(true)
 
   const increment = () => changeCounter(counter + 1)
   const decrement = () => changeCounter(counter - 1)
 
+  const toggle = () => setLit(!isLit)
+
   return (
     <>
       <div className='navbar'>
-        {/* <h3>Counter {isLit}</h3> */}
+        <h3>Counter {(isLit) ? 'light' : 'dark'}</h3>
+        <button onClick={toggle}>sasasas</button>
 
       </div>
       <div className='counter'>
