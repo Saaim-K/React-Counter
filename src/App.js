a5e1016bb450d01cb3e6211929cc530981ebd465
Saaim-K <saaimkhan221@gmail.com>
2022-10-18 23:57:00 +0500

    Counter

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ function App() {
   return (
     <>
       <div>
-        
+
       </div>
       <div>
         <h1>Counter {counter}</h1>
-        <button className="custom-btn btn-12" onClick={() => changeCounter(counter + 1)}><span>CLICK!</span><span>INCREMENT</span></button>
-        <button className="custom-btn btn-12" onClick={() => changeCounter(counter - 1)}><span>CLICK!</span><span>DECREMENT</span></button>
+        <div className='btn'>
+          <button className="custom-btn btn-12" onClick={() => changeCounter(counter + 1)}><span>CLICK!</span><span>INCREMENT</span></button>
+          <button className="custom-btn btn-12" onClick={() => changeCounter(counter - 1)}><span>CLICK!</span><span>DECREMENT</span></button>
+        </div>
       </div>
     </>
   );
