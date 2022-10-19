78d1ea4983ed4b30b6faefed2c275c5384500fdf
Saaim-K <saaimkhan221@gmail.com>
2022-10-19 18:05:43 +0500

    Counter

--- src/App.js
+++ src/App.js
@@ -14,33 +14,14 @@ function App() {
     <>
       <div className={`main-${(isLit) ? 'light' : 'dark'}`}>
         <div className='navbar'>
-          <h3 className={`body-${(isLit) ? 'light' : 'dark'}`}>Counter {(isLit) ? 'light' : 'dark'}</h3>
+          <h3 className={`head-${(isLit) ? 'light' : 'dark'} head` }>Counter</h3>
           <button onClick={toggle}>Toggle Theme</button>
         </div>
-
-
         <div className='counter'>
           <h1 className={`body-${(isLit) ? 'light' : 'dark'}`}>Counter {counter}</h1>
-
-
-
-
-
-
-
-
-
           <div className='btn'>
             <button className={`${(isLit) ? 'custom-btn light-btn' : 'dark-btn'}`} onClick={increment}><span> + 1 </span><span>INCREMENT</span></button>
             <button className={`${(isLit) ? 'custom-btn light-btn' : 'dark-btn'}`} onClick={decrement}><span> - 1 </span><span>DECREMENT</span></button>
-
-
-
-
-
-
-
-
           </div>
         </div>
       </div>
