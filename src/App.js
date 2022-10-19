777cd60778a164ba13262b28a68733506e43c3cf
Saaim-K <saaimkhan221@gmail.com>
2022-10-19 16:51:50 +0500

    Counter

--- src/App.js
+++ src/App.js
@@ -12,16 +12,18 @@ function App() {
 
   return (
     <>
-      <div className='navbar'>
-        <h3 className={`body-${(isLit) ? 'light' : 'dark'}`}>Counter {(isLit) ? 'light' : 'dark'}</h3>
-        <button onClick={toggle}>Toggle Theme</button>
+      <div className={`main-${(isLit) ? 'light' : 'dark'}`}>
+        <div className='navbar'>
+          <h3 className={`body-${(isLit) ? 'light' : 'dark'}`}>Counter {(isLit) ? 'light' : 'dark'}</h3>
+          <button onClick={toggle}>Toggle Theme</button>
 
-      </div>
-      <div className='counter'>
-        <h1 className={`body-${(isLit) ? 'light' : 'dark'}`}>Counter {counter}</h1>
-        <div className='btn'>
-          <button className="custom-btn btn-12" onClick={increment}><span> + 1 </span><span>INCREMENT</span></button>
-          <button className="custom-btn btn-12" onClick={decrement}><span> - 1 </span><span>DECREMENT</span></button>
+        </div>
+        <div className='counter'>
+          <h1 className={`body-${(isLit) ? 'light' : 'dark'}`}>Counter {counter}</h1>
+          <div className='btn'>
+            <button className="custom-btn btn-12" onClick={increment}><span> + 1 </span><span>INCREMENT</span></button>
+            <button className="custom-btn btn-12" onClick={decrement}><span> - 1 </span><span>DECREMENT</span></button>
+          </div>
         </div>
       </div>
     </>
